e2f1c6b59e152a542311726b05bd708fb9e5817a
bobby <sbkim0407@gmail.com>
2019-11-04 18:53:19 +0900

    add model.fit in distribued_training_with_keras.py

--- src/distributed_training_with_keras.py
+++ src/distributed_training_with_keras.py
@@ -44,23 +44,62 @@ train_dataset = mnist_train.map(scale).cache().shuffle(BUFFER_SIZE).batch(BATCH_
 eval_dataset = mnist_test.map(scale).batch(BATCH_SIZE)
 
 # Create the model
-with strategy.scope():
-    model = tf.keras.Sequential([
-        tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(10, activation='softmax')
-    ])
+# with strategy.scope():
+#    model = tf.keras.Sequential([
+#        tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
+#        tf.keras.layers.MaxPooling2D(),
+#        tf.keras.layers.Flatten(),
+#        tf.keras.layers.Dense(64, activation='relu'),
+#        tf.keras.layers.Dense(10, activation='softmax')
+#    ])
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
 
 model.compile(loss='sparse_categorical_crossentropy',
               optimizer=tf.keras.optimizers.Adam(),
               metrics=['accuracy'])
 
 # Define the checkpoint directory to store the checkpoints
-checkpoint_dir = './training_checkpoints'
+checkpoint_dir = '../training_checkpoints'
 # Name of the checkpoint files
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+# Function for decaying the learning rate.
+# You can define any decay function you need.
+def decay(epoch):
+    if epoch < 3:
+        return 1e-3
+    elif (epoch >= 3) and (epoch < 7):
+        return 1e-4
+    else:
+        return 1e-5
+
+# Callback for printing the LR at the end of each epoch.
+class PrintLR(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        print('\nLearning rate for epoch {} is {}'.format(
+            epoch + 1, model.optimizer.lr.numpy()))
+
+callbacks = [
+    tf.keras.callbacks.TensorBoard(log_dir='../logs'),
+    tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,
+                                       save_weights_only=True),
+    tf.keras.callbacks.LearningRateScheduler(decay),
+    PrintLR()
+]
+
+# Train and evaluate
+model.fit(train_dataset, epochs=12, callbacks=callbacks)
+
+
+
+
 
 
 
