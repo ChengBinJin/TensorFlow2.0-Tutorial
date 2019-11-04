50d7d664f20cdc5615ee693ab653bc134420ff48
bobby <sbkim0407@gmail.com>
2019-11-04 09:32:50 +0900

    add model

--- Distributed_training_with_keras.py
+++ Distributed_training_with_keras.py
@@ -40,6 +40,28 @@ def scale(image, label):
 
     return image, label
 
+train_dataset = mnist_train.map(scale).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+eval_dataset = mnist_test.map(scale).batch(BATCH_SIZE)
+
+# Create the model
+with strategy.scope():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+# Define the checkpoint directory to store the checkpoints
+checkpoint_dir = './training_checkpoints'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
 
 
 
