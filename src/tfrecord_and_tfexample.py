5a92353e734fcce08b9632cbcca4827e3fa49a19
chengbin <sbkim0407@gmail.com>
2020-01-04 21:44:34 +0900

    finished tfrecord_and_tfexample.py

--- src/tfrecord_and_tfexample.py
+++ src/tfrecord_and_tfexample.py
@@ -3,7 +3,9 @@
 # dataset performance tips.
 
 # Setup
+import cv2
 import numpy as np
+from IPython.display import display, Image
 import tensorflow as tf
 
 print('Tensorflow version: {}'.format(tf.version.VERSION))
@@ -42,7 +44,7 @@ print('feature.SerializeToString(): {}'.format(feature.SerializeToString()))
 
 # Creating a tf.Example message
 # The number of observations in the dataset.
-n_observations = int(1e4)
+n_observations = int(1e3)
 
 # Boolean feature, encoded as False or True.
 feature0 = np.random.choice([False, True], n_observations)
@@ -97,9 +99,164 @@ print('\ntf.data.Dataset.from_tensor_slices(feature1): {}'.format(tf.data.Datase
 features_dataset = tf.data.Dataset.from_tensor_slices((feature0, feature1, feature2, feature3))
 print('\nfeatures_dataset: {}'.format(features_dataset))
 
+
+def tf_serialize_example(f0, f1, f2, f3):
+    tf_string = tf.py_function(
+        serialize_example,
+        (f0, f1, f2, f3),   # pass these args to the above function
+        tf.string)          # the return type is 'tf.string'
+    return tf.reshape(tf_string, ())  # The result is a scalar
+
 # Use 'take(1)' to only pull one example from the dataset.
 for f0, f1, f2, f3 in features_dataset.take(1):
     print('f0: {}'.format(f0))
     print('f1: {}'.format(f1))
     print('f2: {}'.format(f2))
     print('f3: {}'.format(f3))
+    print(tf_serialize_example(f0, f1, f2, f3))
+
+serialized_features_dataset = features_dataset.map(tf_serialize_example)
+print(serialized_features_dataset)
+
+
+def generator():
+    for features in features_dataset:
+        yield serialize_example(*features)
+
+serialized_features_dataset = tf.data.Dataset.from_generator(
+    generator, output_types=tf.string, output_shapes=())
+
+print(serialized_features_dataset)
+
+filename = './test.tfrecord'
+writer = tf.data.experimental.TFRecordWriter(filename)
+writer.write(serialized_features_dataset)
+
+# Reading a TFRecrod file
+filenames = [filename]
+raw_dataset = tf.data.TFRecordDataset(filenames)
+print('raw_dataset: {}'.format(raw_dataset))
+
+# Note: iterating over a tf.data.Dataset only works with eager execution enabled
+for raw_record in raw_dataset.take(10):
+    print(repr(raw_record))
+
+# Create a description of the feature.
+feature_description = {
+    'feature0': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+    'feature1': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+    'feature2': tf.io.FixedLenFeature([], tf.string, default_value=''),
+    'feature3': tf.io.FixedLenFeature([], tf.float32, default_value=0.0)
+}
+
+def _parse_function(example_proto):
+    # Parse the input 'tf.Example' proto using the dictionary above.
+    return tf.io.parse_single_example(example_proto, feature_description)
+
+parsed_dataset = raw_dataset.map(_parse_function)
+print('\nparsed_dataset: \n{}'.format(parsed_dataset))
+
+for parsed_record in parsed_dataset.take(10):
+    print(repr(parsed_record))
+
+# TFRecrod files in Python
+# Writing a TFRecord file
+with tf.io.TFRecordWriter(filename) as writer:
+    for i in range(n_observations):
+        example = serialize_example(feature0[i], feature1[i], feature2[i], feature3[i])
+        writer.write(example)
+
+# Reading a TFRecrod file
+filenames = [filename]
+raw_dataset = tf.data.TFRecordDataset(filenames)
+print('\nraw_dataset: \n{}'.format(raw_dataset))
+
+for raw_record in raw_dataset.take(1):
+    example = tf.train.Example()
+    example.ParseFromString(raw_record.numpy())
+    print('\nexample: \n{}'.format(example))
+
+# Walkthrough: Reading and writing image data
+# Fetch the images
+cat_in_snow = tf.keras.utils.get_file(
+    '320px-Felis_catus-cat_on_snow.jpg',
+    'https://storage.googleapis.com/download.tensorflow.org/example_images/320px-Felis_catus-cat_on_snow.jpg')
+
+print('\ncat_in_snow: \n{}'.format(cat_in_snow))
+williamsburg_bridge = tf.keras.utils.get_file(
+    '194px-New_East_River_Bridge_from_Brooklyn_det.4a09796u.jpg',
+    'https://storage.googleapis.com/download.tensorflow.org/example_images/194px-New_East_River_Bridge_from_Brooklyn_det.4a09796u.jpg')
+print('\nwilliamsburg_bridege: \n{}'.format(williamsburg_bridge))
+
+img = cv2.imread(cat_in_snow)
+print('img(cat) shape: {}'.format(img.shape))
+cv2.imshow('Cat in Snow', img)
+cv2.waitKey(0)
+
+img = cv2.imread(williamsburg_bridge)
+print('img(bridge) shape: {}'.format(img.shape))
+cv2.imshow('Bridge', img)
+cv2.waitKey(0)
+
+# Write the TFRecord file
+image_labels = {
+    cat_in_snow: 0,
+    williamsburg_bridge: 1
+}
+
+# This is an example, just using the cat iamge.
+image_string = open(cat_in_snow, 'rb').read()
+
+label = image_labels[cat_in_snow]
+
+# Create a dictionary with features that may be relevant.
+def image_example(image_string, label):
+    image_shape = tf.image.decode_jpeg(image_string).shape
+
+    feature = {
+        'height': _int64_feature(image_shape[0]),
+        'width': _int64_feature(image_shape[1]),
+        'depth': _int64_feature(image_shape[2]),
+        'label': _int64_feature(label),
+        'image_raw': _bytes_feature(image_string),
+    }
+
+    return tf.train.Example(features=tf.train.Features(feature=feature))
+
+for line in str(image_example(image_string, label)).split('\n')[:15]:
+    print(line)
+print('...')
+
+# Write the raw image files to 'images.tfrecrods'.
+# First, process the two images into 'tf.Example' messages.
+# Then, write to a '.tfrecords' file.
+record_file = 'images.tfrecords'
+with tf.io.TFRecordWriter(record_file) as writer:
+    for filename, label in image_labels.items():
+        image_string = open(filename, 'rb').read()
+        tf_example = image_example(image_string, label)
+        writer.write(tf_example.SerializeToString())
+
+# Read the TFRecord file
+raw_image_dataset = tf.data.TFRecordDataset('images.tfrecords')
+
+# Create a dictionary describing the features.
+image_feature_description = {
+    'height': tf.io.FixedLenFeature([], tf.int64),
+    'width': tf.io.FixedLenFeature([], tf.int64),
+    'depth': tf.io.FixedLenFeature([], tf.int64),
+    'label': tf.io.FixedLenFeature([], tf.int64),
+    'image_raw': tf.io.FixedLenFeature([], tf.string),
+}
+
+def _parse_image_function(example_proto):
+    # Parse the inptu tf.Example proto using the dictionary above.
+    return tf.io.parse_single_example(example_proto, image_feature_description)
+
+parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
+print('\nparsed_image_dataset: \n{}'.format(parsed_image_dataset))
+
+for index, image_features in enumerate(parsed_image_dataset):
+    image_raw = image_features['image_raw'].numpy()
+    img = Image(data=image_raw)
+    display(img)
